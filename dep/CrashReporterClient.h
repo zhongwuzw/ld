a3c64a6af51730dde39a7620a889186a1ae301dd
zhongwuzw <zhongwuzw@qq.com>
2021-07-09 17:41:48 +0800

    Add dep

---
+++ dep/CrashReporterClient.h
@@ -0,0 +1,11 @@
+#define CRASHREPORTER_ANNOTATIONS_SECTION "Any"
+
+const char* const CRASHREPORTER_ANNOTATIONS_VERSION = "";
+
+struct crashreporter_annotations_t
+{
+	const char* a;
+	int b, c, d, e, f, g;
+};
+
+#define CRSetCrashLogMessage(m)
